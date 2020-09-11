b73083faaf79b5a8e5be1b0e5c2fe5aa1bc41baf
pawelgrimm <developer@pawelgrimm.com>
2020-09-11 10:41:28 -0400

    add project search component

---
+++ src/components/project-search/project-search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectSearch = () => {
+  return <input type="text" placeholder="Enter a project" />;
+};
+
+export default ProjectSearch;
---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ProjectSearch from "../project-search/project-search";
+
+const Timer = () => {
+  return (
+    <div id="timer">
+      <ProjectSearch />
+      <div id="timer" />
+      <div id="buttons">
+        <button id="button">start</button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
