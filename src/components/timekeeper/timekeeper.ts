571f2eb89dd2e9b84f7f1251741d47f71defe471
pawelgrimm <developer@pawelgrimm.com>
2020-09-11 18:04:53 -0400

    create timer based on web worker

---
+++ src/components/timekeeper/timekeeper.ts
@@ -0,0 +1,100 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import * as workerPath from "file-loader?name=[name].js!./worker-script";
+
+const DEBUG = true;
+
+declare function setInterval(cb: () => void, to: number): number;
+
+export enum TimerError {
+  MISSING_INTERVAL = "MISSING_INTERVAL",
+  INTERVAL_NOT_NUMBER = "INTERVAL_NOT_NUMBER",
+}
+
+//export declare function postMessage(message: TimerWorkerMessage): void;
+
+export interface TimerWorkerMessage {
+  clock?: number;
+  error?: TimerError;
+}
+
+/**
+ * Write a debug message to the console
+ * @param message the message to write
+ */
+export const postDebugMessage = (message: string): void => {
+  if (DEBUG) {
+    // tslint:disable-next-line:no-console
+    console.log(`[DEBUG]: ${message}`);
+  }
+};
+
+/**
+ * Add a listener to the worker's message event.
+ * @param worker the worker to listen to
+ * @param cb the function to execute
+ */
+export function listenToWorker(
+  worker: Worker,
+  cb: (data: TimerWorkerMessage) => void
+): void {
+  worker.addEventListener("message", ({ data }) => cb(data));
+}
+
+/**
+ * Instruct the worker to start the clock with a given update interval
+ * @param worker the worker
+ * @param interval refresh interval in ms
+ */
+const startWorker = (worker: Worker, interval: number): void => {
+  worker.postMessage({ type: "SET_INTERVAL", payload: interval });
+};
+
+/**
+ * Instruct the worker to stop running the timer
+ * @param worker the worker
+ */
+const pauseWorker = (worker: Worker): void => {
+  worker.postMessage({ type: "PAUSE" });
+};
+
+/**
+ * Enlist a web worker to report current time at a given interval
+ * @param interval the interval in ms
+ * @return the current time
+ */
+export const useTimeKeeper = (
+  interval: number = 100
+): [number, () => void, () => void] => {
+  const worker = useMemo(() => new Worker(workerPath), []);
+  const [time, setTime] = useState<number>(Date.now());
+
+  const pause = useCallback(() => pauseWorker(worker), [worker]);
+  const start = useCallback(() => startWorker(worker, interval), [
+    worker,
+    interval,
+  ]);
+
+  useEffect(() => {
+    console.log("useTimeKeeper render");
+  });
+
+  useEffect(() => {
+    console.log("timekeeper effect ran");
+    listenToWorker(worker, ({ clock, error }) => {
+      if (error) {
+        postDebugMessage(error);
+      } else if (clock) {
+        setTime(clock);
+      }
+    });
+
+    return () => worker.terminate();
+  }, [worker]);
+
+  // useEffect(() => {
+  //   startWorker(worker, interval);
+  // }, [interval, worker]);
+
+  return [time, start, pause];
+};
--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectSearch from "../project-search/project-search";
+import Button from "../button/button";
+import { useTimeKeeper } from "../timekeeper/timekeeper";
 
 const Timer = () => {
+  const [time, setTime] = useState<number>(15 * 60);
+  // const [sessionStart, setSessionStart] = useState<number>(0);
+  const [inProgress, setInProgress] = useState<boolean>(false);
+  const [clock, startTimer, pauseTimer] = useTimeKeeper();
+
+  useEffect(() => {
+    if (inProgress) {
+      setTime((prev) => prev - 1);
+    }
+  }, [clock, inProgress]);
+
+  useEffect(() => {});
+
+  const onStartStopClick = () => {
+    if (inProgress) {
+      setTime(15 * 60);
+    }
+    onToggleClick();
+  };
+
+  const onToggleClick = () => {
+    inProgress ? pauseTimer() : startTimer();
+    setInProgress((prev) => !prev);
+  };
+
   return (
     <div id="timer">
       <ProjectSearch />
-      <div id="timer" />
+      <div id="timer">{`${Math.floor(time / 60)}:${Math.abs(time % 60)}`}</div>
       <div id="buttons">
-        <button id="button">start</button>
+        <Button onClick={onStartStopClick}>
+          {inProgress ? "Stop" : "Start"}
+        </Button>
+        <Button onClick={onToggleClick}>
+          {inProgress ? "Pause" : "Resume"}
+        </Button>
       </div>
     </div>
   );
