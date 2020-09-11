624bdcfb2918703b79bbc4950a3ef213c6e4d381
pawelgrimm <developer@pawelgrimm.com>
2020-09-11 10:32:25 -0400

    updated header src dir and created new basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText("Pomi")).toBeInTheDocument();
 });
