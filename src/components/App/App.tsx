108a985abf12c781fe8e2c2c3f4a0dab5aa2d885
pawelgrimm <developer@pawelgrimm.com>
2020-09-28 09:57:24 -0400

    add css normalization

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from "./App.module.scss";
+import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { EditSession, Header } from "../index";
 import Timer from "../Timer/timer";
+import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <CssBaseline />
       <div className={styles.app}>
         <Header />
         <div className={styles.mainContent}>
