ce6109c5f0c2a7f9295153ed41b9c682ddd8fb75
pawelgrimm <developer@pawelgrimm.com>
2020-09-28 10:10:04 -0400

    basic Material UI theme config

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
+import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { EditSession, Header } from "../index";
 import Timer from "../Timer/timer";
 import styles from "./App.module.scss";
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#ffd803",
+      contrastText: "#272343",
+    },
+    secondary: {
+      main: "#f25f4c",
+      contrastText: "#272343",
+    },
+  },
+});
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <CssBaseline />
-      <div className={styles.app}>
-        <Header />
-        <div className={styles.mainContent}>
-          <Switch>
-            <Route path="/fudge">
-              <EditSession />
-            </Route>
-            <Route path="/">
-              <Timer />
-            </Route>
-          </Switch>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CssBaseline />
+        <div className={styles.app}>
+          <Header />
+          <div className={styles.mainContent}>
+            <Switch>
+              <Route path="/fudge">
+                <EditSession />
+              </Route>
+              <Route path="/">
+                <Timer />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 };
 
