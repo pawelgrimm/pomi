b77fc7a6fcac9cb969b60d7ee278dd10be9ebdae
pawelgrimm <developer@pawelgrimm.com>
2020-10-27 14:47:18 -0400

    Add static folder for serving index.html

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,11 @@ import { mountRoutes } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("build"));
 mountRoutes(app);
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html");
+});
 
 export default app;
