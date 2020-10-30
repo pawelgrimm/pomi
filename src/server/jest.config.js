676f7e25b9a1e3c95cb2a86386d1c55b15db3d0b
pawelgrimm <developer@pawelgrimm.com>
2020-10-30 17:34:58 -0400

    fixup! change some jest/ts setup for running server tests

--- src/server/jest.config.js
+++ src/server/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  notify: true,
+  setupFiles: ["./setupTest.js"],
   globals: {
     "ts-jest": {
       tsconfig: "src/server/tsconfig.json",
