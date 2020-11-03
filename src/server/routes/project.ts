22f8259fa2a4ba502264bf0f052e41fbc500c278
pawelgrimm <developer@pawelgrimm.com>
2020-11-03 17:53:36 -0500

    create tests for project routes WIP

--- src/server/routes/project.ts
+++ src/server/routes/project.ts
@@ -1,27 +1,37 @@
 import Router from "express-promise-router";
-import { Projects } from "../db";
-import { validateProject } from "../../shared/validators";
-import { authenticate } from "../middleware/authenticate";
 import QueryString from "qs";
+import { authenticate, validationErrorHandler } from "../middleware";
+import { Projects } from "../db";
+import { SelectOptions } from "../db/queries";
+import { validateProject } from "../../shared/validators";
 
 const router = Router();
 
 const parseSelectAllOptions = (
   options: QueryString.ParsedQs
-): { sync_token: string; include_archived: boolean } => {
+): SelectOptions => {
   const { sync_token, include_archived } = options;
   return {
-    sync_token: (sync_token && sync_token.toString()) || "*",
-    include_archived: !!include_archived,
+    syncToken: (sync_token && sync_token.toString()) || "*",
+    includeArchived: !!include_archived,
   };
 };
 
+router.use(authenticate);
+
+/*      NEW PROJECT      */
+router.post("/", async (req, res) => {
+  const { userId } = res.locals;
+  const project = await Projects.create(userId, validateProject(req.body));
+  res.status(201).send({ project });
+});
+
 /*      GET ALL PROJECTS FOR USER     */
-router.get("/", authenticate, async (req, res) => {
+router.get("/", async (req, res) => {
   const { userId } = res.locals;
   const options = parseSelectAllOptions(req.query);
   try {
-    const projects = await Projects.selectAll(userId, options);
+    const projects = await Projects.select(userId, options);
     res.status(200).send({ projects });
   } catch {
     res.status(500).send();
@@ -29,26 +39,17 @@ router.get("/", authenticate, async (req, res) => {
 });
 
 /*      GET PROJECT BY ID    */
-router.get("/:id", authenticate, async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { userId } = res.locals;
   const id = req.params.id;
   try {
-    const project = await Projects.selectOneById(userId, id);
+    const project = await Projects.selectOne(userId, id);
     res.status(200).send({ project });
   } catch {
     res.status(500).send();
   }
 });
 
-/*      NEW PROJECT      */
-router.post("/", authenticate, async (req, res) => {
-  const { userId } = res.locals;
-  try {
-    const project = await Projects.create(userId, validateProject(req.body));
-    res.status(201).send({ project });
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+router.use(validationErrorHandler);
 
 export default router;
--- src/server/routes/tests/project.test.ts
+++ src/server/routes/tests/project.test.ts
@@ -1,46 +1,90 @@
 import request from "supertest";
 import app from "../../server";
-import { Projects, close } from "../../db";
+import { Projects, close, pool } from "../../db";
+import { v4 as uuid } from "uuid";
+import { resetTestDb } from "../../setupTest";
+import { sql } from "slonik";
+import { UserModel } from "../../../shared/models";
+
+let user: UserModel;
+
+beforeAll(() => {
+  user = {
+    id: uuid(),
+    display_name: "projectsTestUser",
+    email: "projects@example.com",
+  };
+
+  return new Promise(async (resolve) => {
+    await resetTestDb();
+    await pool.query(
+      sql`INSERT INTO users(id, display_name, email) VALUES (${user.id}, ${user.display_name}, ${user.email})`
+    );
+    resolve();
+  });
+});
 
 afterAll(() => {
   close().then();
 });
 
-const userId = "8e290323-6f92-441a-a946-efff804944cf";
-
-const testProject = {
+const validProject = {
   title: "a test project",
+  isArchived: true,
 };
 
-describe("Project create tests", () => {
+describe("Authorization", () => {
+  it("should require authorization on all paths", async (done) => {
+    await request(app).post("/api/projects").expect(401);
+    await request(app).get("/api/projects").expect(401);
+    await request(app).get("/api/projects/123").expect(401);
+    done();
+  });
+});
+
+describe("POST project/", () => {
   it("should create a project", async (done) => {
     const { body } = await request(app)
       .post("/api/projects")
-      .set("Authorization", `Bearer ${userId}`)
-      .send(testProject)
+      .set("Authorization", `Bearer ${user.id}`)
+      .send(validProject)
       .expect(201);
 
-    expect(body).toContainEqual({
-      project: {
-        id: expect.any(String),
-        isArchived: false,
-        ...testProject,
-      },
-    });
-
-    expect(await Projects.selectAll(userId)).toContainEqual({
-      project: {
-        id: expect.any(String),
-        isArchived: false,
-        ...testProject,
-      },
-    });
+    expect(body).toEqual(
+      expect.objectContaining({
+        project: {
+          id: expect.any(String),
+          ...validProject,
+        },
+      })
+    );
     done();
   });
+
   it("should not create a project", async (done) => {
-    const res = await request(app).post("/api/projects").expect(400);
+    const { body } = await request(app)
+      .post("/api/projects")
+      .set("Authorization", `Bearer ${user.id}`)
+      .send({ isArchived: "black beans" })
+      .expect(422);
 
-    expect(res.body.errors.length).toBeGreaterThanOrEqual(1);
+    expect(body).toEqual(
+      expect.objectContaining({
+        errors: expect.any(Array),
+      })
+    );
     done();
   });
+});
+
+describe("GET projects/", () => {
+  it.todo("Should get all projects");
+  it.todo("Should get new projects");
+  it.todo("Should get projects including archived projects");
+  it.todo("Should return correctly when user has no projects");
+});
+
+describe("GET projects/:id", () => {
+  it.todo("Should get specific projects");
+  it.todo("Should return correctly when project is not found");
 });
