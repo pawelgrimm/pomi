6ee487a896d309937d3725d787de43a4279a9e88
pawelgrimm <developer@pawelgrimm.com>
2020-11-03 11:13:28 -0500

    fixup! update projects tests with new utility fxns

--- src/shared/utils/testing-helpers/index.ts
+++ src/shared/utils/testing-helpers/index.ts
@@ -6,8 +6,16 @@ export const sleep = (duration: number) => {
   return new Promise((resolve) => setTimeout(resolve, duration));
 };
 
+/**
+ * Wrap an array of objects in expect.objectContaining
+ * @param objects an array of objects
+ */
 export const wrapObjectContaining = (objects: {}[]) =>
   objects.map((object) => expect.objectContaining(object));
 
+/**
+ * Wrap an array of objects in expect.objectContaining and wrap that in expect.arrayContaining
+ * @param objects an array of objects
+ */
 export const arrayContainingObjectsContaining = (objects: {}[]) =>
   expect.arrayContaining(wrapObjectContaining(objects));
