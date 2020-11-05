dd81dcd4c38244330f6b59296ddd61d06b9fa410
pawelgrimm <developer@pawelgrimm.com>
2020-11-05 15:22:04 -0500

    equip the new camelCaseQueryParams middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { mountRoutes } from "./routes";
 import { logError } from "./middleware";
+import { camelCaseQueryParams } from "./middleware/shared";
 
 const app = express();
 app.use(express.json());
 app.use(express.static("build"));
+
+app.use(camelCaseQueryParams);
+
 mountRoutes(app);
 
 app.get("/*", function (req, res) {
