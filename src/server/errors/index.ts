3afdb24e98fd3c063d83936c766ca46f16248192
pawelgrimm <developer@pawelgrimm.com>
2020-11-05 11:59:20 -0500

    create new ParseOptions error

---
+++ src/server/errors/index.ts
@@ -0,0 +1,17 @@
+type Path = {
+  name: string;
+  message?: string;
+};
+
+export class ParseOptionsError extends Error {
+  private paths: Path[];
+
+  constructor(paths: Path[]) {
+    super();
+    this.name = "ParseOptionsError";
+    this.paths = paths;
+    this.message = paths
+      .map((path) => `${path.name}: ${path.message}`)
+      .join("\n");
+  }
+}
