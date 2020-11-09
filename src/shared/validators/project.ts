53e1f62f77be06b9213f15bbe8214a02e038105a
pawelgrimm <developer@pawelgrimm.com>
2020-11-09 14:44:33 -0500

    update docs for project validator

--- src/shared/validators/project.ts
+++ src/shared/validators/project.ts
@@ -2,17 +2,22 @@ import Joi from "joi";
 
 import { ProjectModel } from "../types";
 
+/**
+ * A Joi schema representing a ProjectModel
+ */
 const projectSchema = Joi.object({
   id: Joi.string().uuid({ version: "uuidv4" }).optional(),
   title: Joi.string().max(255).optional(),
   isArchived: Joi.boolean().optional(),
 });
 
+/**
+ * Validate and parse an object into as a Project
+ * @param project an object representing a project
+ */
 export const validateProject = (project: any): ProjectModel => {
   return Joi.attempt(
     project,
-    projectSchema.options({
-      stripUnknown: true,
-    })
+    projectSchema.options({ stripUnknown: true })
   ) as ProjectModel;
 };
