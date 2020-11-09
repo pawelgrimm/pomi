7bd47afb55882000f8dbc478e357b0cf245c7ec9
pawelgrimm <developer@pawelgrimm.com>
2020-11-09 15:00:00 -0500

    finish task validator test

--- src/shared/validators/tests/task.test.ts
+++ src/shared/validators/tests/task.test.ts
@@ -1,12 +1,10 @@
-import { validateTask } from "../task";
 import { TaskModel } from "../../types";
+import { validateTask } from "../task";
 
 let validTask: TaskModel;
 
 beforeEach(() => {
-  validTask = {
-    /* TODO: add properties from TaskModel */
-  };
+  validTask = {};
 });
 
 describe("Task Validator ", () => {
@@ -14,24 +12,50 @@ describe("Task Validator ", () => {
     const validatedTask = validateTask(validTask);
     expect(validatedTask).toEqual(validTask);
   });
-  it("should not pass validation when a required field is missing", () => {
-    const { /* TODO: add a required field */, ...rest } = validTask;
-    expect(() => validateTask({ ...rest })).toThrow(
-      /"/* TODO: add a required field */" is required/
-    );
+
+  it("Should convert isCompleted = `false` to boolean", () => {
+    const completedTask = validateTask({ ...validTask, isCompleted: "false" });
+    expect(completedTask).toEqual({ isCompleted: false });
   });
+
+  it("Should require isCompleted to be a boolean", () => {
+    const invalidTask = { ...validTask, isCompleted: "yes" };
+    expect(() => validateTask(invalidTask)).toThrow(/must be a boolean/);
+  });
+
   it("Should not strip optional fields", () => {
-    /* TODO: intialize optional field */
-    const optionalField = new Object();
-    const taskWithOptionalField = { optionalField, ...validTask };
+    const title = "my title";
+    const taskWithOptionalField = { title, ...validTask };
     const strippedTask = validateTask(taskWithOptionalField);
-    expect(strippedTask).toMatchObject({ optionalField });
+    expect(strippedTask).toMatchObject({ title });
   });
+
   it("Should strip unknown fields", () => {
     const strippedTask = validateTask({
       extra: "junk",
       ...validTask,
     });
     expect(strippedTask).not.toMatchObject({ extra: "junk" });
+  });
+
+  it("Should not along very long titles", () => {
+    const title = "".padEnd(256, "Z");
+    expect(() => validateTask({ ...validTask, title })).toThrow(
+      /"title" length must be less than or equal to/
+    );
+  });
+
+  it("Should not allow invalid projectIds", () => {
+    const projectId = "fbbf7f5a-bad5-4f49-9862-16ddb45f1a6dZZZ";
+    expect(() => validateTask({ ...validTask, projectId })).toThrow(
+      /"projectId" must be a valid GUID/
+    );
+  });
+
+  it("Should not allow invalid ids", () => {
+    const id = "fbbf7f5a-bad5-4f49-9862-16ddb45f1a6dZZZ";
+    expect(() => validateTask({ ...validTask, id })).toThrow(
+      /"id" must be a valid GUID/
+    );
   });
 });
