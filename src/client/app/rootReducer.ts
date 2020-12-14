3003f975272f71774fb44b8b0a2d5c956f592d7a
pawelgrimm <developer@pawelgrimm.com>
2020-12-14 16:27:32 -0500

    create projects slice

--- src/client/app/rootReducer.ts
+++ src/client/app/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../services/auth/auth";
+import projectsReducer from "../features/state/projectsSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  projects: projectsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
