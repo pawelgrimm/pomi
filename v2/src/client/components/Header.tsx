db4a40a1e4081047a6d4a97b7757ad9ce6bf2914
pawelgrimm <developer@pawelgrimm.com>
2021-05-02 17:53:41 -0400

    🛠 Set up eslint and prettier

--- v2/src/client/components/Header.tsx
+++ v2/src/client/components/Header.tsx
@@ -1,38 +1,45 @@
 import React from "react";
-import {AppBar, Toolbar, Typography, WithStyles, withStyles} from "@material-ui/core";
-import {createStyles, Theme} from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  WithStyles,
+  withStyles,
+} from "@material-ui/core";
+import { createStyles, Theme } from "@material-ui/core/styles";
 
 type HeaderProps = WithStyles<typeof styles>;
 
-function Header({classes}: HeaderProps) {
-    return (
-        <AppBar position="static" className={classes.headerContent}>
-            <Toolbar>
-                <Typography variant="h1" className={classes.brand}>
-                    Pomi
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    );
+function Header({ classes }: HeaderProps) {
+  return (
+    <AppBar position="static" className={classes.headerContent}>
+      <Toolbar>
+        <Typography variant="h1" className={classes.brand}>
+          Pomi
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-const styles = (({ palette, typography, spacing}: Theme) => createStyles({
+const styles = ({ palette, typography, spacing }: Theme) =>
+  createStyles({
     headerContent: {
-        alignItems: "start",
-        backgroundColor: palette.primary.main,
-        color: palette.primary.contrastText,
-        display: "flex",
-        justifyContent: "space-between",
-        padding: spacing(1),
+      alignItems: "start",
+      backgroundColor: palette.primary.main,
+      color: palette.primary.contrastText,
+      display: "flex",
+      justifyContent: "space-between",
+      padding: spacing(1),
     },
     brand: {
-        backgroundColor: palette.primary.main,
-        color: palette.primary.contrastText,
-        fontSize: "2.3rem",
-        fontWeight: typography.fontWeightMedium,
-        padding: 0,
-        margin: 0,
+      backgroundColor: palette.primary.main,
+      color: palette.primary.contrastText,
+      fontSize: "2.3rem",
+      fontWeight: typography.fontWeightMedium,
+      padding: 0,
+      margin: 0,
     },
-}));
+  });
 
 export default withStyles(styles)(Header);
