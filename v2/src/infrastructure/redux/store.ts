cfef42a53135dc3619c4705ad0e41f07adfa361f
pawelgrimm <developer@pawelgrimm.com>
2021-10-06 13:39:13 -0500

    Remove store serializable check in redux

--- v2/src/infrastructure/redux/store.ts
+++ v2/src/infrastructure/redux/store.ts
@@ -3,6 +3,13 @@ import rootReducer, { RootState } from "./rootReducer";
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["projects/init/fulfilled", "projects/add/fulfilled"],
+        ignoredPaths: ["projects"],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
