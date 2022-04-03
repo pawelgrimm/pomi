4f5af8547f9913f66acdb9f3a065def4ab2ca664
Paweł Grimm <pawel@doist.com>
2022-04-02 19:00:41 -0500

    feat: Read sessions from database

--- app/components/Timer/SessionsListPane.tsx
+++ app/components/Timer/SessionsListPane.tsx
@@ -8,30 +8,49 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { useEffect, useState } from "react";
+import { supabase } from "../../utils/supabaseClient";
 
 type Session = {
   task: string;
-  length: string;
+  length_mins: number;
   notes: string;
   tags: string[];
 };
 
-const sessions: Session[] = [
-  {
-    task: "Gucci alligator 2",
-    length: "30m",
-    notes: "I did this thing for a while",
-    tags: ["productive", "distracted"],
-  },
-  {
-    task: "I'm baby toast",
-    length: "1h16m",
-    notes: "I did this other thing for a while",
-    tags: ["enjoyed", "distracted"],
-  },
-];
+// const sessions: Session[] = [
+//   {
+//     task: "Gucci alligator 2",
+//     length: "30m",
+//     notes: "I did this thing for a while",
+//     tags: ["productive", "distracted"],
+//   },
+//   {
+//     task: "I'm baby toast",
+//     length: "1h16m",
+//     notes: "I did this other thing for a while",
+//     tags: ["enjoyed", "distracted"],
+//   },
+// ];
 
 export function SessionsListPane() {
+  const [sessions, setSessions] = useState<Session[]>([]);
+
+  useEffect(() => {
+    supabase
+      .from<Session>("sessions")
+      .select()
+      .then(({ data, error }) => {
+        if (error) {
+          console.log(error);
+        }
+
+        if (data != null) {
+          setSessions(data);
+        }
+      });
+  }, []);
+
   return (
     <Card>
       <CardContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
@@ -44,7 +63,7 @@ export function SessionsListPane() {
   );
 }
 
-function SessionCard({ task, length, notes, tags }: Session) {
+function SessionCard({ task, length_mins, notes, tags }: Session) {
   return (
     <Card>
       <CardContent sx={{ display: "flex", flexDirection: "column" }}>
@@ -59,7 +78,7 @@ function SessionCard({ task, length, notes, tags }: Session) {
             {task}
           </Typography>
           <Typography variant="overline" color="text.secondary">
-            {length}
+            {formatLength(length_mins)}
           </Typography>
         </Box>
         <Typography variant="subtitle1" color="text.secondary" mb={2} mt={1}>
@@ -73,4 +92,14 @@ function SessionCard({ task, length, notes, tags }: Session) {
       </CardContent>
     </Card>
   );
+}
+
+function formatLength(lengthMins: number) {
+  const hours = Math.floor(lengthMins / 60);
+  const mins = lengthMins % 60;
+
+  const hoursPart = hours ? `${hours}h` : "";
+  const minsPart = mins ? `${mins}m` : "";
+
+  return hoursPart + minsPart;
 }
